21932346ea662d902e2461ef646e698cc502bb33
davidovitch <david.verelst@gmail.com>
2013-01-06 18:37:59 +0100

    significant update to the config file, adding disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,21 +1,58 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+"""Settings for pelican."""
+
+# This can also be the absolute path to a theme that you downloaded
+# i.e. './themes/anothertheme/'
+THEME = 'notmyidea'
+
+# The folder ``images`` should be copied into the folder ``static`` when
+# generating the output.
+STATIC_PATHS = ['images', ]
+
+# See http://pelican.notmyidea.org/en/latest/settings.html#timezone
+TIMEZONE = 'Europe/Paris'
+DEFAULT_LANG = u'en'
+
+# I like to put everything into the category ``Blog``, which also appears on
+# the main menu. Tags will not appear on the menu.
+DEFAULT_CATEGORY = 'Blog'
 
 AUTHOR = u'David Verelst'
 SITENAME = u'dada-blog'
-SITEURL = ''
+SITEURL = u'http://davidovitch.github.com/dada-blog'
+GITHUB_URL = u'https://github.com/davidovitch'
+FEED_DOMAIN = SITEURL
+FEED_MAX_ITEMS = 20
 
-TIMEZONE = 'Europe/Paris'
+WITH_PAGINATION = True
+DEFAULT_PAGINATION = 10
+REVERSE_ARCHIVE_ORDER = True
 
-DEFAULT_LANG = u'en'
+# Uncomment what ever you want to use
+#GOOGLE_ANALYTICS = 'XX-XXXXXXX-XX'
+DISQUS_SITENAME = 'dada-blog'
+#TWITTER_USERNAME = 'username'
+
+# A list of the extensions that the Markdown processor will use. Refer to the
+# extensions chapter in the Python-Markdown documentation for a complete list
+# of supported extensions.
+MD_EXTENSIONS = (['codehilite','extra'])
+
+
+# List of templates that are used directly to render content. Typically direct
+# templates are used to generate index pages for collections of content
+# (e.g. tags and category index pages).
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives'))
+
+SOCIAL = (('Github', 'https://github.com/davidovitch'),)
 
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
           ('Jinja2', 'http://jinja.pocoo.org'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# I like to have ``Archives`` in the main menu.
+MENUITEMS = (
+    ('Archives', '{0}/archives.html'.format(SITEURL)),
+    )
 
-DEFAULT_PAGINATION = 10
+
